843511b06b47c5b028e9ff895634ff3def814f19
Raghuveer Talari <raghu.veer12529@gmail.com>
2021-02-05 18:33:58 +0530

    checkout

--- ecommerce/src/Checkout.js
+++ ecommerce/src/Checkout.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./Checkout.css";
 
 function Checkout() {
-    return (
+  return (
+    <div className="checkout">
+      <div className="checkout_left">
+        <img
+          className="checkout_ad"
+          src="https://images.iphonephotographyschool.com/7179/1120/Panoramic-iPhone-Photos-4.jpg"
+          alt=""
+        />
+
         <div>
-            
+            <h2 className="checkout__title">
+                Your Shopping Basket
+            </h2>
         </div>
-    )
+        div.checkpout_right
+      </div>
+    </div>
+  );
 }
 
-export default Checkout
+export default Checkout;
